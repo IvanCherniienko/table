4e5610804862e7b1ebd096c4bf85a3a9871d175c
IvanCherniienko <111380678+IvanCherniienko@users.noreply.github.com>
2023-02-28 19:32:04 +0200

    fix

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-let background = document.getElementsByClassName("background")
-let massive = Array.from(background)
+// let background = document.getElementsByClassName("background")
+// let massive = Array.from(background)
 
-for (let i = 0; i < massive.length; i++) {
-    const element = massive[i];
-    element.style.backgroundColor = 'red'
+// for (let i = 0; i < massive.length; i++) {
+//     const element = massive[i];
+//     element.style.backgroundColor = 'red'
+// }
+
+let tableArr = document.querySelector('table');
+
+for (let i = 0; i < tableArr.rows.length; i++) {
+  let row = tableArr.rows[i];
+  row.cells[i].style.backgroundColor = 'red';
 }
-
